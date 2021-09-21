29fa472cf30a71a7534eb719349b0e2fd440d83a
dovv <@@dvs1234561>
2021-09-21 08:26:42 -0300

    aula 3.3

--- projetos/section_3/src/App.tsx
+++ projetos/section_3/src/App.tsx
@@ -1,17 +1,22 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
+import useCounter from "./hooks/useCounter";
+import PeopleList from "./PeopleList";
 import ScrollableBox, { ScrollableRef } from "./ScrollableBox";
 
 function App() {
+  {/*
   const [names, setNames] = useState([
     'Daniel', 'Thiago', 'Alex',
     'Jão', 'Alex'
   ])
+  */}
 
   const boxRef = useRef<ScrollableRef>(null);
   const [content, setContent] = useState<string>();
   const [postId, setPostId] = useState(1);
 
+  {/* 
   // atende componentDidMount, componentDidUpdate e componentWillAmount
   // executa no momento que o componente é montado, gera efeitos colaterais
   useEffect(() => {
@@ -21,13 +26,16 @@ function App() {
     }
   }, [])
 
-   // executa no momento que o componente é inicializado e atualizado
+  // executa no momento que o componente é inicializado e atualizado
    useEffect(() => {
     console.log('componente foi montado')
     return () => {
       console.log('componente foi desmontado')
     }
   }, [names])
+  */}
+
+  const number = useCounter();
 
   const fetchData = useCallback(
     async function () {
@@ -50,6 +58,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* 
         <ul>
           {
             names.map((name, index) => <li key={index}>{name}</li>)
@@ -62,8 +71,15 @@ function App() {
         >
           adicionar jeremias
         </button>
-        
-        {postId}
+        */}
+
+        { number }
+        <br/>
+
+        <PeopleList />
+        <PeopleList />
+
+        { postId }
         <ScrollableBox ref={boxRef} width={120} height={120}>
           <p>{content || "carregando..."}</p>
         </ScrollableBox>

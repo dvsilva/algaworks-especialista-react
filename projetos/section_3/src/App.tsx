882f19ffd5510ee9bfcc7879473b698d4623e186
dovv <@@dvs1234561>
2021-09-22 08:27:51 -0300

    aula 3.10

--- projetos/section_3/src/App.tsx
+++ projetos/section_3/src/App.tsx
@@ -11,81 +11,72 @@ import useCounter from './hooks/useCounter';
 import PeopleList from './PeopleList';
 import ScrollableBox, { ScrollableRef } from './ScrollableBox';
 
-/* 
-interface InitialState { count: number }
-
-type Action =
-  { type: 'DECREMENT' } |
-  { type: 'INCREMENT', payload: number }
-
-const initialState: InitialState = {
-  count: 1
+interface InitialState {
+  count: number;
 }
 
-function reducer (state: InitialState, action: Action): InitialState {
+type Action = { type: 'DECREMENT' } | { type: 'INCREMENT'; payload: number };
+
+const initialState: InitialState = {
+  count: 1,
+};
+
+function reducer(state: InitialState, action: Action): InitialState {
   switch (action.type) {
     case 'INCREMENT':
       return {
-        count: state.count + action.payload
-      }
+        count: state.count + action.payload,
+      };
     case 'DECREMENT':
       return {
-        count: state.count -1 
-      }
+        count: state.count - 1,
+      };
     default:
-      return state
+      return state;
   }
-} 
-*/
+}
 
 function App() {
-  {
-    /*
   const [names, setNames] = useState([
-    'Daniel', 'Thiago', 'Alex',
-    'Jão', 'Alex'
-  ])
-  */
-  }
+    'Daniel',
+    'Thiago',
+    'Alex',
+    'Jão',
+    'Alex',
+  ]);
 
-  {
-    /* 
   // atende componentDidMount, componentDidUpdate e componentWillAmount
   // executa no momento que o componente é montado, gera efeitos colaterais
   useEffect(() => {
-    console.log('componente foi montado')
+    console.log('componente foi montado');
     return () => {
-      console.log('componente foi desmontado')
-    }
-  }, [])
+      console.log('componente foi desmontado');
+    };
+  }, []);
 
   // executa no momento que o componente é inicializado e atualizado
-   useEffect(() => {
-    console.log('componente foi montado')
+  useEffect(() => {
+    console.log('componente foi montado');
     return () => {
-      console.log('componente foi desmontado')
-    }
-  }, [names])
-  */
-  }
+      console.log('componente foi desmontado');
+    };
+  }, [names]);
 
-  /* 
-  const number = useCounter(9)
+  const number = useCounter(9);
 
   // const counter = { current: 2 }
-  const counter = useRef(2)
-  const div = useRef<HTMLDivElement>(null)
+  const counter = useRef(2);
+  const div = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     console.log(div.current);
-    
-    if (div.current) {
-      div.current.style.backgroundColor = 'green'
-    }
-  }, [])
-*/
 
-  /*  const [state, dispatch] = useReducer(reducer, initialState) */
+    if (div.current) {
+      div.current.style.backgroundColor = 'green';
+    }
+  }, []);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const boxRef = useRef<ScrollableRef>(null);
   const [content, setContent] = useState<string>();
@@ -110,52 +101,55 @@ function App() {
   const data = useMemo(() => Array(33_000_000).fill({ foo: 'bar' }), []);
 
   return (
-    <div className='App' /* ref={div} */>
-      {/* 
+    <div className='App' ref={div}>
       <ul>
-        {
-          names.map((name, index) => <li key={index}>{name}</li>)
-        }
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
       </ul>
       <button
         onClick={() => {
-          setNames([...names, 'jeremias'])
+          setNames([...names, 'jeremias']);
         }}
       >
         adicionar jeremias
       </button>
-      */}
 
-      {/*
-      { number } 
-      
-      <div style={{ backgroundColor: 'peachpuff' }}>
-        { counter.current }
+      <div>{number}</div>
+
+      <div
+        style={{
+          backgroundColor: 'peachpuff',
+        }}
+      >
+        {counter.current}
       </div>
 
       <PeopleList />
-      <PeopleList /> 
-      */}
+      <PeopleList />
 
-      {/* 
-      <div style={{ backgroundColor: 'peachpuff' }}>
-        { state.count }
+      <div
+        style={{
+          backgroundColor: 'peachpuff',
+        }}
+      >
+        {state.count}
       </div>
+
       <button
         onClick={() => {
-          dispatch({ type: 'INCREMENT', payload: 2 })
+          dispatch({ type: 'INCREMENT', payload: 2 });
         }}
       >
         acrescer
       </button>
       <button
         onClick={() => {
-          dispatch({ type: 'DECREMENT' })
+          dispatch({ type: 'DECREMENT' });
         }}
       >
         diminuir
-      </button> 
-      */}
+      </button>
 
       {postId}
       <ScrollableBox ref={boxRef} width={120} height={120}>

42166169ece928de8e1ccdf0548a7b2ce743211f
dovv <@@dvs1234561>
2021-09-24 08:13:12 -0300

    aula 4.22

--- projetos/section_4/alganews-cms/src/components/Table/Table.tsx
+++ projetos/section_4/alganews-cms/src/components/Table/Table.tsx
@@ -57,11 +57,13 @@ export default function Table () {
       {
         Header: 'Column 1',
         accessor: 'col1',
-        width: 320
+        width: 320,
+        Cell: (row) => <div style={{ textAlign: 'right' }}>{row.value}</div>
       },
       {
         Header: 'Column 2',
         accessor: 'col2',
+        Cell: (row) => <div style={{ textAlign: 'center' }}>{row.value}</div>
       },
       {
         Header: 'Ações',

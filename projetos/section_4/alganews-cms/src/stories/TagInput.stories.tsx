71d17532937b59eb96400a9317e9994f1144cfd3
dovv <@@dvs1234561>
2021-09-28 08:12:37 -0300

    aula 4.51

--- projetos/section_4/alganews-cms/src/stories/TagInput.stories.tsx
+++ projetos/section_4/alganews-cms/src/stories/TagInput.stories.tsx
@@ -16,3 +16,17 @@ Default.args = {
   placeholder: 'Insira as tags deste post',
   tags: [{ id: '1', text: 'JavaScript' }]
 }
+
+export const VariousTags = Template.bind({})
+VariousTags.args = {
+  placeholder: 'Insira as tags deste post',
+  tags: [
+    { id: '1', text: 'JavaScript' },
+    { id: '2', text: 'Java' },
+    { id: '3', text: 'Ruby on Rails' },
+    { id: '4', text: 'Python' },
+    { id: '5', text: 'JavaScript' },
+    { id: '6', text: 'JavaScript' },
+    { id: '7', text: 'JavaScript' },
+  ]
+}

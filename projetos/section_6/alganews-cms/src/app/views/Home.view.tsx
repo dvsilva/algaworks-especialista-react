0ea412d1d85a297098e8647a56758aaa9de5632b
dovv <@@dvs1234561>
2021-10-01 07:59:08 -0300

    aula 6.4

--- projetos/section_6/alganews-cms/src/app/views/Home.view.tsx
+++ projetos/section_6/alganews-cms/src/app/views/Home.view.tsx
@@ -1,8 +1,12 @@
 import usePageTitle from "../../core/hooks/usePageTitle"
+import DefaultLayoyt from "../layouts/Default"
 
 export default function Home () {
   usePageTitle('Home')
-  return <div>
+  return <DefaultLayoyt>
     <h1>Home</h1>
-  </div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse incidunt eligendi iure ut rerum, distinctio quisquam sed culpa minima ipsa.
+    </p>
+  </DefaultLayoyt>
 }

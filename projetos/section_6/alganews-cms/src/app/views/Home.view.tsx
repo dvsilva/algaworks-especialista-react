05658286928fab44b25c47f24ff1df4063f2f80f
dovv <@@dvs1234561>
2021-10-01 08:17:46 -0300

    aula 6.8

--- projetos/section_6/alganews-cms/src/app/views/Home.view.tsx
+++ projetos/section_6/alganews-cms/src/app/views/Home.view.tsx
@@ -1,13 +1,15 @@
 import usePageTitle from "../../core/hooks/usePageTitle"
 import PostList from "../features/PostsList"
-import UserMetrics from "../features/UserMetrics"
+import UserPerformance from "../features/UserPerformance"
+import UserTopTags from "../features/UserTopTags"
 import DefaultLayout from "../layouts/Default"
 
 export default function Home () {
   usePageTitle('Home')
 
   return <DefaultLayout>
-    <UserMetrics />
+    <UserTopTags />
+    <UserPerformance />
     <PostList />
   </DefaultLayout>
 }

a3fe38b9d330205c2f52009d8d97e84fc5ab036b
dovv <@@dvs1234561>
2021-10-02 10:41:53 -0300

    aula 6.22

--- projetos/section_6/alganews-cms/src/app/features/EditorProfile.tsx
+++ projetos/section_6/alganews-cms/src/app/features/EditorProfile.tsx
@@ -8,7 +8,9 @@ interface EditorProfileProps {
   hidePersonalData?: boolean;
 }
 
-function EditorProfile (props: EditorProfileProps) {
+function EditorProfile(props: EditorProfileProps) {
+  // throw new Error("Houve um erro ao renderizar o componente da aplicação");
+
   return <EditorProfileWrapper>
     <EditorHeadline>
       <Avatar src={'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'} />

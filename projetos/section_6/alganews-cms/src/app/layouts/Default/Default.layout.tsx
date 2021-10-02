14603dff896de8146233e344c342062b9bd28b14
dovv <@@dvs1234561>
2021-10-02 10:14:20 -0300

    aula 6.18

--- projetos/section_6/alganews-cms/src/app/layouts/Default/Default.layout.tsx
+++ projetos/section_6/alganews-cms/src/app/layouts/Default/Default.layout.tsx
@@ -1,4 +1,5 @@
 import confirm from '../../../core/utils/confirm'
+import info from '../../../core/utils/info'
 import Logo from '../../components/Logo'
 import NavBar from '../../components/NavBar'
 import SessionController from '../../components/SessionController'
@@ -27,7 +28,13 @@ function DefaultLayout (props: DefaultLayoutProps) {
           onLogout={() => {
             confirm({
               title: 'Voce quer delogar?',
-              onConfirm: () => window.alert('batata'),
+              onConfirm: () => {
+                info({
+                  title: 'Você foi deslogado',
+                  description: 'Você será redirecionado para a página de login'
+                })
+                console.log('executado')
+              },
               onCancel: () => window.alert('laranja'),
             })
           }}

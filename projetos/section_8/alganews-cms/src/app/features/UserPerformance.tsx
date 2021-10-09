857cf757df17b23e2265afe4c3af9cd22169c7c7
dovv <@@dvs1234561>
2021-10-09 13:14:33 -0300

    aula 8.35

--- projetos/section_8/alganews-cms/src/app/features/UserPerformance.tsx
+++ projetos/section_8/alganews-cms/src/app/features/UserPerformance.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react"
+import Skeleton from "react-loading-skeleton"
 import withBoundary from "../../core/hoc/withBoundary"
 import transformEditorMonthlyEaningsIntoChartJs from "../../core/utils/transformEditorMonthlyEarningsIntoChartJs"
 import MetricService from "../../sdk/services/Metric.service"
 import Chart, { ChartProps } from "../components/Chart/Chart"
-
 function UserPerformance () {
   const [editorEarnings, setEditorEarings] = useState<ChartProps['data']>()
   const [error, setError] = useState<Error>()
@@ -22,10 +22,12 @@ function UserPerformance () {
     throw error
 
   if (!editorEarnings)
-    return null
+    return <div>
+      <Skeleton height={227} />
+    </div>
 
   return <Chart
-    title="batata"
+    title="Média de performance nos últimos 12 meses"
     data={editorEarnings}
   />
 }

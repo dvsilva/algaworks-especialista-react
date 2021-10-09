3a3cd5f52b9540bf59450550138a323664b8f52e
dovv <@@dvs1234561>
2021-10-09 13:41:19 -0300

    aula 8.38

--- projetos/section_8/alganews-cms/src/app/components/ImageUpload/ImageUpload.tsx
+++ projetos/section_8/alganews-cms/src/app/components/ImageUpload/ImageUpload.tsx
@@ -3,6 +3,7 @@ import Icon from '@mdi/react'
 import { ChangeEvent, useState } from 'react'
 import FileService from '../../../sdk/services/File.service'
 import Button from '../Button/Button'
+import Loading from '../Loading'
 import * as IU from './ImageUpload.styles'
 
 export interface ImageUploadProps {
@@ -10,19 +11,25 @@ export interface ImageUploadProps {
   onImageUpload: (imageUrl: string) => any
 }
 
-function ImageUpload (props: ImageUploadProps) {
+function ImageUpload(props: ImageUploadProps) {
   const [filePreview, setFilePreview] = useState<string | null>(null)
+  const [pushing, setPushing] = useState(false)
 
-  function handleChange (e: ChangeEvent<HTMLInputElement>) {
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
     const file = e.target.files![0]
-    
+
     if (file) {
       const reader = new FileReader()
 
       reader.addEventListener('load', async e => {
-        setFilePreview(String(e.target?.result));
-        const imageUrl = await FileService.upload(file)
-        props.onImageUpload(imageUrl)
+        try {
+          setPushing(true)
+          setFilePreview(String(e.target?.result));
+          const imageUrl = await FileService.upload(file)
+          props.onImageUpload(imageUrl)
+        } finally {
+          // setPushing(false)
+        }
       })
 
       reader.readAsDataURL(file)
@@ -31,22 +38,24 @@ function ImageUpload (props: ImageUploadProps) {
 
   if (filePreview)
     return <IU.ImagePreviewWrapper>
+      <Loading show={pushing} />
       <IU.ImagePreview preview={filePreview}>
         <Button
           variant={'primary'}
           label={'Remover imagem'}
-          onClick={() => setFilePreview(null)}  
+          onClick={() => setFilePreview(null)}
         />
       </IU.ImagePreview>
     </IU.ImagePreviewWrapper>
 
   return <IU.Wrapper>
+    <Loading show={pushing} />
     <IU.Label>
       <Icon
         size={'24px'}
         path={mdiUpload}
       />
-      { props.label }
+      {props.label}
       <IU.Input
         type="file"
         onChange={handleChange}

c0e866c4e5b539fef035fdd0a159770445f77d4e
dovv <@@dvs1234561>
2021-10-16 07:59:49 -0300

    aula 10.14

--- projetos/section_10/alganews-cms/src/core/store/Post.slice.ts
+++ projetos/section_10/alganews-cms/src/core/store/Post.slice.ts
@@ -1,12 +1,20 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createSlice,
+  isFulfilled,
+  isPending,
+  PayloadAction,
+} from "@reduxjs/toolkit";
 import { Post } from "../../sdk/@types";
 import PostService from "../../sdk/services/Post.service";
 
 interface PostSliceState {
   paginated?: Post.Paginated;
+  fetching: boolean;
 }
 
 const initialState: PostSliceState = {
+  fetching: false,
   paginated: {
     page: 0,
     size: 0,
@@ -31,6 +39,18 @@ const postSlice = createSlice({
     addPost(state, action: PayloadAction<Post.Summary>) {
       state.paginated?.content?.push(action.payload);
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.paginated = action.payload;
+      })
+      .addMatcher(isPending, (state) => {
+        state.fetching = true;
+      })
+      .addMatcher(isFulfilled, (state) => {
+        state.fetching = false;
+      });
   },
 });
 

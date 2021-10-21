0382ff38a534dcd03a20726d9969bfd84e9ef3b9
dovv <@@dvs1234561>
2021-10-21 07:31:12 -0300

    aula 11.12

--- projetos/section_11/alganews-blog/pages/index.tsx
+++ projetos/section_11/alganews-blog/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
 export default function Home(props: any) {
@@ -13,7 +12,6 @@ export default function Home(props: any) {
       </Head>
 
       <main className={styles.main}>
-        <Header />
         <h1 className={styles.title}>
           Aprendendo <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -53,19 +51,6 @@ export default function Home(props: any) {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
